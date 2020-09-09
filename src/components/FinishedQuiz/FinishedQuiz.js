b21dacaf167868880d41faac662bb3b86f0fc478
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-09 17:18:36 +0400

    Component. lesson13 is done

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
-import classes from './FinishedQuiz.css'
+import classes from './FinishedQuiz.css';
+import Button from "../UI/Button/Button";
 
 const FinishedQuiz = (props) => {
   let successCount = 0;
@@ -28,7 +29,11 @@ const FinishedQuiz = (props) => {
       </ul>
       <p>Правильно {successCount} из {props.quiz.length}</p>
       <div>
-        <button onClick={props.onRetry}>Повторить</button>
+        <Button
+          onClick={props.onRetry}
+          type="primary"
+        >Повторить</Button>
+        <Button  type="success">Перейти в список тестов</Button>
       </div>
     </div>
   )
