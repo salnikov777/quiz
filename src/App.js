f25c722fadcc07a4eda892e2e6a85a52d50ecc86
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-09 12:19:31 +0400

    Component. lesson7 is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 function App() {
   return (
     <Layout>
-
+      <Quiz/>
     </Layout>
   );
 }
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import classes from './Quiz.css'
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+
+class Quiz extends Component{
+  state = {
+    quiz: [
+      {
+        question: 'Какого цвета небо?',
+        rightAnswerId: 2,
+        answers: [
+          {text: 'Черный', id: 1},
+          {text: 'Синий', id: 2},
+          {text: 'Красный', id: 3},
+          {text: 'Зеленый', id: 4},
+        ]
+      }
+    ]
+  }
+
+  onAnswerClickHandler = (answerId) => {
+    console.log('Answer ID:' , answerId);
+  }
+
+  render() {
+    return(
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[0].answers}
+            question={this.state.quiz[0].question}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Quiz;
