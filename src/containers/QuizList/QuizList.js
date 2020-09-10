b1a88ccd88134df9f96c2875432781df54483659
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-10 11:29:30 +0400

    Routing. Lesson 2 is done!

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import classes from './QuizList.css'
+
+export default class QuizList extends Component{
+  render() {
+    return(
+      <div>
+        <h1>QuizList</h1>
+      </div>
+    );
+  }
+}
