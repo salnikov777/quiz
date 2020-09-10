e99a7cd494495444ee7ea5961416a7f64e851abf
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-10 13:34:06 +0400

    Forms. Lesson 1 is done!

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,38 @@
 import React, {Component} from "react";
 import classes from './Auth.css'
+import Button from "../../components/UI/Button/Button";
 
-export default class Auth extends Component{
+export default class Auth extends Component {
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  }
+
   render() {
-    return(
-      <div>
-        <h1>Auth</h1>
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler}>
+            <input type="text"/>
+            <input type="text"/>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >Войти</Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >Зарегистрироваться</Button>
+          </form>
+        </div>
       </div>
     );
   }
