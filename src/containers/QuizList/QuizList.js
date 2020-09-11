715640e9174828f6cbaff7067e6476e8d14ddbba
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-11 16:54:33 +0400

    Server. Lesson 3 is completed!

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -4,14 +4,18 @@ import {NavLink} from "react-router-dom";
 import axios from 'axios'
 
 export default class QuizList extends Component {
+  state = {
+    quizes: []
+  }
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, idx) => {
+    return this.state.quizes.map((quiz) => {
       return (
         <li
-          key={idx}
+          key={quiz.id}
         >
-          <NavLink to={'/quiz/' + quiz}>
-            Test {quiz}
+          <NavLink to={'/quiz/' + quiz.id}>
+            {quiz.name}
           </NavLink>
 
         </li>
@@ -19,10 +23,22 @@ export default class QuizList extends Component {
     });
   }
 
-  componentDidMount() {
-    axios.get('https://react-quiz-2020-91576.firebaseio.com/quiz.json').then((response)=>{
-      console.log(response);
-    });
+  async componentDidMount() {
+    try{
+      const response = await axios.get('https://react-quiz-2020-91576.firebaseio.com/quizes.json');
+      const quizes = [];
+      Object.keys(response.data).forEach((key, index)=>{
+        quizes.push({
+          id: key,
+          name: `Тест № ${index + 1}`
+        });
+      })
+      this.setState({
+        quizes
+      })
+    }catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
