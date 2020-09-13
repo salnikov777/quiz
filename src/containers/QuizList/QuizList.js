1a71c4d266c832e687c67ce1e527bf6e5ab49bd7
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-13 19:06:37 +0400

    Server. Lesson 4 is completed!

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -2,10 +2,12 @@ import React, {Component} from "react";
 import classes from './QuizList.css'
 import {NavLink} from "react-router-dom";
 import axios from 'axios'
+import Loader from "../../components/UI/Loader/Loader";
 
 export default class QuizList extends Component {
   state = {
-    quizes: []
+    quizes: [],
+    loading: true
   }
 
   renderQuizes() {
@@ -34,7 +36,8 @@ export default class QuizList extends Component {
         });
       })
       this.setState({
-        quizes
+        quizes,
+        loading: false
       })
     }catch (e) {
       console.log(e);
@@ -46,9 +49,13 @@ export default class QuizList extends Component {
       <div className={classes.QuizList}>
         <div>
           <h1>Список тестов</h1>
-          <ul>
-            {this.renderQuizes()}
-          </ul>
+          {
+            this.state.loading
+            ? <Loader/>
+            :<ul>
+                {this.renderQuizes()}
+              </ul>
+          }
         </div>
 
       </div>
