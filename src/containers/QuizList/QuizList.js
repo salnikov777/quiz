a52a80e39dfb4c3d1b92ef7f7244235f33245146
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-14 12:26:23 +0400

    Redux. Lesson 2 is completed!

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,17 +1,14 @@
 import React, {Component} from "react";
 import classes from './QuizList.css'
 import {NavLink} from "react-router-dom";
-import axios from '../../axios/axios-quiz'
 import Loader from "../../components/UI/Loader/Loader";
+import {connect} from 'react-redux'
+import {fetchQuizes} from "../../store/actions/quiz";
 
-export default class QuizList extends Component {
-  state = {
-    quizes: [],
-    loading: true
-  }
+class QuizList extends Component {
 
   renderQuizes() {
-    return this.state.quizes.map((quiz) => {
+    return this.props.quizes.map((quiz) => {
       return (
         <li
           key={quiz.id}
@@ -25,23 +22,24 @@ export default class QuizList extends Component {
     });
   }
 
-  async componentDidMount() {
-    try{
-      const response = await axios.get('/quizes.json');
-      const quizes = [];
-      Object.keys(response.data).forEach((key, index)=>{
-        quizes.push({
-          id: key,
-          name: `Тест № ${index + 1}`
-        });
-      })
-      this.setState({
-        quizes,
-        loading: false
-      })
-    }catch (e) {
-      console.log(e);
-    }
+  componentDidMount() {
+    this.props.fetchQuizes()
+    // try{
+    //   const response = await axios.get('/quizes.json');
+    //   const quizes = [];
+    //   Object.keys(response.data).forEach((key, index)=>{
+    //     quizes.push({
+    //       id: key,
+    //       name: `Тест № ${index + 1}`
+    //     });
+    //   })
+    //   this.setState({
+    //     quizes,
+    //     loading: false
+    //   })
+    // }catch (e) {
+    //   console.log(e);
+    // }
   }
 
   render() {
@@ -50,7 +48,7 @@ export default class QuizList extends Component {
         <div>
           <h1>Список тестов</h1>
           {
-            this.state.loading
+            this.props.loading && this.props.quizes.length !== 0
             ? <Loader/>
             :<ul>
                 {this.renderQuizes()}
@@ -62,3 +60,18 @@ export default class QuizList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return{
+    quizes: state.quiz.quizes,
+    loading: state.quiz.loading
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return{
+    fetchQuizes: () => dispatch(fetchQuizes())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList)
--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import thunk from 'redux-thunk';
+import {createStore, compose , applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from "./store/reducers/rootReducer";
 import {BrowserRouter} from "react-router-dom";
 
-const app = (<React.StrictMode>
-                <BrowserRouter>
-                  <App/>
-                </BrowserRouter>
-            </React.StrictMode>
-)
 
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+
+  );
+
+
+
+const app = (<React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+
+)
 
 
 ReactDOM.render(
   app
-,
-document.getElementById('root')
+  ,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
